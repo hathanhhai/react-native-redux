0a85a08167b1e95ebecd5569214af11a5ccee621
hathanhhai <darkwinqw3@gmail.com>
2018-02-20 10:58:01 +0700

    master

---
+++ components/ComA.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import { View, Text } from 'react-native';
+
+export default class ComA extends Component {
+    render() {
+        return (
+            <View style={{width:150,height:150,backgroundColor:"blue"}}>
+            </View>
+        )
+    }
+}
